46a1803cfabc88136fbc6770df62670cf0b7e1f0
Kyanh Pham <kyanh.pham@mths.ca>
2022-09-14 14:40:24 +0000

    added copyright

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,8 +1,11 @@
+//"copyright [2022] <Kyanh Pham>" [legal / copyright][5]//
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!\n";
-    std::cout << "Finished My Work\n";
+    int
+    main() {
+        std::cout << "Hello, World!\n";
+        std::cout << "Finished My Work\n";
 
-    std::cout << "\nDone.\n";
-}
+        std::cout << "\nDone.\n";
+    }
